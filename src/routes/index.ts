26bb949f5d7bf5f4113214c05c98aaca65515633
neiva <neivacristiano@yahoo.com.br>
2020-03-11 09:39:22 -0300

    initializing acad

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Request, Response, Router } from 'express'
+
+
+const apiRoutes = Router()
+
+apiRoutes.get('/', (req: Request, res: Response) => {
+    res.send('Academia Service full cracked 2k88')
+})
+
+export default apiRoutes
