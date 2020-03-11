9d3d9abaf644a9566b720232fd2c87e9d088a040
neiva <neivacristiano@yahoo.com.br>
2020-03-11 10:25:01 -0300

    typing

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,7 @@
 import { userModel } from "../models/academia"
+import { Client } from "../interfaces/users"
 
-export const insertUser =  async (data: any) => {
+export const insertUser =  async (data: Client) => {
     const insertResponse = await userModel.insertMany([data])
 
     return insertResponse
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response, Router } from 'express'
 import { insertUser } from '../controllers/user'
+import { Client } from '../interfaces/users'
 
 const routes = Router()
 
 routes.post('/insertCliente', async (req: Request, res: Response, next: NextFunction) => {
 
-    const data = req.body
+    const data: Client = req.body
     const insertResult = await insertUser(data)
 
     return res.status(200).json({
